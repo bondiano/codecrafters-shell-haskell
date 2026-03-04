8b99fa26521020e6b17cbe32bbfdab9037a3fb38
bondiano <batbondik0@gmail.com>
2026-03-04 22:47:34 +0700

    codecrafters submit [skip ci]

--- test/Test.hs
+++ test/Test.hs
@@ -1,56 +1,50 @@
 module Main where
 
 import Shell.Parser (parseArgs)
-import System.Exit (exitFailure, exitSuccess)
-
-data TestResult = Pass | Fail String String String
-
-runTest :: String -> [String] -> TestResult
-runTest input expected =
-  let actual = parseArgs input
-   in if actual == expected
-        then Pass
-        else Fail input (show expected) (show actual)
-
-tests :: [(String, [String])]
-tests =
-  [ -- basic splitting
-    ("hello world", ["hello", "world"])
-  , ("hello    world", ["hello", "world"])
-  , ("", [])
-  , ("   ", [])
-  , -- single quotes preserve spaces
-    ("'hello world'", ["hello world"])
-  , ("'hello    world'", ["hello    world"])
-  , ("'world     test'", ["world     test"])
-  , -- adjacent quotes concatenate
-    ("'hello''world'", ["helloworld"])
-  , ("hello''world", ["helloworld"])
-  , ("a'b'c", ["abc"])
-  , -- empty quotes
-    ("''", [""])
-  , -- mixed quoted and unquoted
-    ("echo 'hello world'", ["echo", "hello world"])
-  , ("cat '/tmp/file name'", ["cat", "/tmp/file name"])
-  , ("a 'b c' d", ["a", "b c", "d"])
-  , -- from the challenge examples
-    ("echo 'shell hello'", ["echo", "shell hello"])
-  , ("cat '/tmp/file name' '/tmp/file name with spaces'", ["cat", "/tmp/file name", "/tmp/file name with spaces"])
-  ]
+import Test.Tasty
+import Test.Tasty.HUnit
 
 main :: IO ()
-main = do
-  let results = map (\(input, expected) -> runTest input expected) tests
-      failures = [(i, e, a) | Fail i e a <- results]
-      total = length results
-      passed = total - length failures
+main = defaultMain tests
 
-  mapM_
-    ( \(input, expected, actual) ->
-        putStrLn $ "FAIL: parseArgs " ++ show input ++ "\n  expected: " ++ expected ++ "\n  actual:   " ++ actual
-    )
-    failures
-
-  putStrLn $ "\n" ++ show passed ++ "/" ++ show total ++ " tests passed"
-
-  if null failures then exitSuccess else exitFailure
+tests :: TestTree
+tests =
+  testGroup
+    "parseArgs"
+    [ testGroup
+        "basic splitting"
+        [ testCase "two words" $ parseArgs "hello world" @?= ["hello", "world"]
+        , testCase "multiple spaces" $ parseArgs "hello    world" @?= ["hello", "world"]
+        , testCase "empty string" $ parseArgs "" @?= []
+        , testCase "only spaces" $ parseArgs "   " @?= []
+        ]
+    , testGroup
+        "single quotes"
+        [ testCase "preserves spaces" $ parseArgs "'hello world'" @?= ["hello world"]
+        , testCase "preserves multiple spaces" $ parseArgs "'hello    world'" @?= ["hello    world"]
+        , testCase "challenge example" $ parseArgs "'world     test'" @?= ["world     test"]
+        ]
+    , testGroup
+        "adjacent quotes"
+        [ testCase "concatenates quoted" $ parseArgs "'hello''world'" @?= ["helloworld"]
+        , testCase "empty quotes in word" $ parseArgs "hello''world" @?= ["helloworld"]
+        , testCase "quotes inside word" $ parseArgs "a'b'c" @?= ["abc"]
+        ]
+    , testGroup
+        "empty quotes"
+        [ testCase "standalone empty quotes" $ parseArgs "''" @?= [""]
+        ]
+    , testGroup
+        "mixed"
+        [ testCase "echo with quoted arg" $ parseArgs "echo 'hello world'" @?= ["echo", "hello world"]
+        , testCase "cat with quoted path" $ parseArgs "cat '/tmp/file name'" @?= ["cat", "/tmp/file name"]
+        , testCase "quoted in middle" $ parseArgs "a 'b c' d" @?= ["a", "b c", "d"]
+        ]
+    , testGroup
+        "challenge examples"
+        [ testCase "echo shell hello" $ parseArgs "echo 'shell hello'" @?= ["echo", "shell hello"]
+        , testCase "cat with two paths" $
+            parseArgs "cat '/tmp/file name' '/tmp/file name with spaces'"
+              @?= ["cat", "/tmp/file name", "/tmp/file name with spaces"]
+        ]
+    ]
